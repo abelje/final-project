196622a21040249f23d48a48cc4d3520b52d9443
abelje <161378812+abelje@users.noreply.github.com>
2025-12-08 20:51:11 -0600

    created a photoresistor class

--- photoresist.py
+++ photoresist.py
@@ -1,3 +1,18 @@
-class Photoresistor:
-    def __init__(self):
-        pass
+from machine import Pin,PWM,ADC,reset
+import time
+
+pwm = PWM(Pin(25, Pin.OUT), 1000)
+adc = ADC(Pin(13))
+adc.atten(ADC.ATTN_11DB)
+adc.width(ADC.WIDTH_10BIT)
+
+try:
+    while True:
+        adcValue = adc.read()
+        pwm.duty(adcValue)
+        print(adc.read())
+        time.sleep_ms(100)
+
+except:
+    pwm.deinit()
+    reset()
--- photoresistor.py
+++ photoresistor.py
@@ -1,17 +1,31 @@
-from machine import Pin,PWM,ADC
+from machine import Pin,PWM,ADC, reset
 import time
 
-pwm = PWM(Pin(25, Pin.OUT), 1000)
-adc = ADC(Pin(13))
-adc.atten(ADC.ATTN_11DB)
-adc.width(ADC.WIDTH_10BIT)
+class Photoresistor:
+    def __init__(self, pwm_pin, adc_pin):
+        self.pwm = PWM(Pin(pwm_pin, Pin.OUT), 1000)
+        self.adc = ADC(Pin(adc_pin))
+        self.adc.atten(ADC.ATTN_11DB)
+        self.adc.width(ADC.WIDTH_10BIT)
 
-try:
-    while True:
-        adcValue = adc.read()
-        pwm.duty(adcValue)
-        print(adc.read())
-        time.sleep_ms(100)
+    def read(self):
+        return self.adc.read()
 
-except:
-    pwm.deinit()
+    def duty(self, value):
+        self.pwm.duty(value)
+
+    def deinit(self):
+        self.pwm.deinit()
+        reset()
+
+if __name__ == '__main__':
+    try:
+        resistor = Photoresistor(25, 13)
+        while True:
+            adcValue = resistor.read()
+            resistor.duty(adcValue)
+            print(resistor.read())
+            time.sleep_ms(100)
+
+    except:
+        resistor.deinit()
