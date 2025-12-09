80b6875fb3f621fd78c7d5d249407919e891a024
abelje <161378812+abelje@users.noreply.github.com>
2025-12-09 15:39:58 -0600

    new version of photoresistor.py to be more friendly to deinitializing leds.

--- photoresistor.py
+++ photoresistor.py
@@ -3,19 +3,35 @@ from led import Led
 from machine import ADC, Pin
 
 class Photoresistor:
-    def __init__(self, adc_pin, led, delay=50, timer_id=1):
+    def __init__(self, adc_pin, delay=50, timer_id=1):
         self.adc = ADC(Pin(adc_pin))
         self.adc.atten(ADC.ATTN_11DB)
         self.adc.width(ADC.WIDTH_10BIT)
-        self.led = led
+        
+        self.led = None
         self.delay = delay
         self.timer = Timer(timer_id)
+        self.running= False
+    
+    def start_dim(self, led):
+        print("started!")
+        self.led = led
+        self.running = True
+        self.timer.init(period=self.delay, callback=self._update)
         
-        self.timer.init(period=delay, callback=self.update)
-
-    def update(self, t):
-        value = self.adc.read()
-        self.led.brightness(value / 1023)
+    def stop_dim(self):
+        self.running = False
+        self.timer.deinit()    
+    
+    def _update(self, timer):
+        if self.running:
+            value = self.adc.read()
+            self.led.brightness(value / 1023)
+        
+    def deinit(self):
+        self.stop_dim()
+        self.led = None
+        self.adc = None
 
     def read(self):
         return self.adc.read()
@@ -23,4 +39,6 @@ class Photoresistor:
         
 if __name__ == '__main__':
     led = Led(25)
-    resistor = Photoresistor(13, led)
+    resistor = Photoresistor(13)
+    resistor.start_dim(led)
+
